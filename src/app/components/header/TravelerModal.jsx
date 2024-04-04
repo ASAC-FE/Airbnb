62a931d76db59081f67be5c7cf3472d6a9ef6899
honi31 <winner020531@naver.com>
2024-04-04 11:22:31 +0900

    [FEAT]-SearchBar 입력값 수정

--- src/app/components/header/TravelerModal.jsx
+++ src/app/components/header/TravelerModal.jsx
@@ -1,7 +1,7 @@
 'use client'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PetModal from '@/app/components/header/PetModal'
 import Plus from '/public/travelermodal/plus.svg'
 import Minus from '/public/travelermodal/minus.svg'
@@ -13,12 +13,12 @@ function useCount(initialCount) {
     setCount((prevCount) => prevCount + 1)
   }
   const onDecrease = () => {
-    setCount((prevCount) => Math.max(prevCount - 1, -1, 0)) // 최솟값 0 설정
+    setCount((prevCount) => Math.max(prevCount - 1, 0)) // 최솟값 0 설정
   }
   return [count, onIncrease, onDecrease]
 }
 
-export default function TravelerModal({ anchorEl, setAnchorEl }) {
+export default function TravelerModal({ anchorEl, setAnchorEl, onCountChange }) {
   // traveler 내용
   const [adultCount, onIncreaseAdultCount, onDecreaseAdultCount] = useCount(0)
   const [childCount, onIncreaseChildCount, onDecreaseChildCount] = useCount(0)
@@ -31,7 +31,9 @@ export default function TravelerModal({ anchorEl, setAnchorEl }) {
   const onClose = () => {
     setAnchorEl(null)
   }
-
+  const handleCountChange = () => {
+    onCountChange([adultCount, childCount, babyCount, petCount]);
+  };
   return (
     <Menu
       id="traveler-modal"
@@ -68,14 +70,20 @@ export default function TravelerModal({ anchorEl, setAnchorEl }) {
 
           <div className="flex justify-between w-28 items-center">
             <button
-              onClick={onDecreaseAdultCount}
+              onClick={() => {
+                onDecreaseAdultCount();
+                handleCountChange();
+              }}
               className={adultCount!==0 ? "items-center p-2 border rounded-full border-gray-400 text-gray-500 hover:border-black hover:text-black" : "text-gray-200 border-gray-200 items-center p-2 border rounded-full"}
             >
               <Minus width="18" height="18"/>
             </button>
             <span className="p-4">{adultCount}</span>
             <button
-              onClick={onIncreaseAdultCount}
+              onClick={() => {
+                onIncreaseAdultCount();
+                handleCountChange();
+              }}
               className="items-center p-2 border rounded-full border-gray-400 text-gray-500 hover:border-black hover:text-black"
             >
               <Plus width="18" height="18"/> 
@@ -94,14 +102,20 @@ export default function TravelerModal({ anchorEl, setAnchorEl }) {
 
           <div className="flex justify-between w-28 items-center">
             <button
-              onClick={onDecreaseChildCount}
+              onClick={() => {
+              onDecreaseChildCount();
+              handleCountChange();
+              }}
               className={childCount!==0 ? "items-center p-2 border rounded-full border-gray-400 text-gray-500 hover:border-black hover:text-black" : "text-gray-200 border-gray-200 items-center p-2 border rounded-full"}
             >
               <Minus width="18" height="18"/>
             </button>
             <span className="p-4">{childCount}</span>
             <button
-              onClick={onIncreaseChildCount}
+              onClick={() => {
+              onIncreaseChildCount();
+              handleCountChange();
+              }}
               className="items-center p-2 border rounded-full border-gray-400 text-gray-500 hover:border-black hover:text-black"
             >
               <Plus width="18" height="18"/>
@@ -120,14 +134,20 @@ export default function TravelerModal({ anchorEl, setAnchorEl }) {
 
           <div className="flex justify-between w-28 items-center">
             <button
-              onClick={onDecreaseBabyCount}
+              onClick={() => {
+                onDecreaseBabyCount();
+                handleCountChange();
+                }}
               className={babyCount!==0 ? "items-center p-2 border rounded-full border-gray-400 text-gray-500 hover:border-black hover:text-black" : "text-gray-200 border-gray-200 items-center p-2 border rounded-full"}
             >
               <Minus width="18" height="18"/>
             </button>
             <span className="p-4">{babyCount}</span>
             <button
-              onClick={onIncreaseBabyCount}
+              onClick={() => {
+                onIncreaseBabyCount();
+                handleCountChange();
+                }}
               className="items-center p-2 border rounded-full border-gray-400 text-gray-500 hover:border-black hover:text-black"
             >
               <Plus width="18" height="18"/>
@@ -152,14 +172,20 @@ export default function TravelerModal({ anchorEl, setAnchorEl }) {
 
           <div className="flex justify-between w-28 items-center">
             <button
-              onClick={onDecreasePetCount}
+              onClick={() => {
+                onDecreasePetCount();
+                handleCountChange();
+                }}
               className={petCount!==0 ? "items-center p-2 border rounded-full border-gray-400 text-gray-500 hover:border-black hover:text-black" : "text-gray-200 border-gray-200 items-center p-2 border rounded-full"}
             >
               <Minus width="18" height="18"/>
             </button>
             <span className="p-4">{petCount}</span>
             <button
-              onClick={onIncreasePetCount}
+              onClick={() => {
+                onIncreasePetCount();
+                handleCountChange();
+                }}
               className="items-center p-2 border rounded-full border-gray-400 text-gray-500 hover:border-black hover:text-black"
             >
               <Plus width="18" height="18"/>
