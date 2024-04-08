99b27f4b7f412df3767460dce25ff5b1faea3f8e
podol0207 <audrhks0207@naver.com>
2024-04-08 18:57:19 +0900

    [FEAT]-카카오로그인

--- src/app/api/home/route.js
+++ src/app/api/home/route.js
@@ -1,6 +1,6 @@
-import RoomsData from '@/app/lib/data.json'
+import RoomsData from "@/app/lib/data.json";
 export async function GET() {
-  const roomsList = RoomsData.data.rooms
+  const roomsList = RoomsData.data.rooms;
   const set = roomsList.map((room) => ({
     id: parseInt(room.id),
     image: room.images,
@@ -11,7 +11,7 @@ export async function GET() {
     date: room.date,
     description: room.description,
     rating: room.rating,
-  }))
+  }));
 
-  return Response.json({ data: JSON.stringify(set) })
+  return Response.json({ data: JSON.stringify(set) });
 }
