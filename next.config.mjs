59322a95bcffcce1cbfe095884225d478d27b877
honi31 <winner020531@naver.com>
2024-04-10 18:13:09 +0900

    [FEAT]-api

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   webpack: (config) => {
     config.module.rules.push({
       test: /.svg$/,
@@ -10,14 +10,14 @@ const nextConfig = {
 
     return config;
   },
-  async rewrites() {
-    return [
-      {
-        source: "/members/new", // POST 요청을 보낼 경로를 일치
-        destination: "http://localhost:8080/members/new", // 대상 경로로 Spring의 POST Mapping 되어있는 주소
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/members/new", // POST 요청을 보낼 경로를 일치
+  //       destination: "http://localhost:8080/members/new", // 대상 경로로 Spring의 POST Mapping 되어있는 주소
+  //     },
+  //   ];
+  // },
   images: {
     remotePatterns: [
       {
--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -1,8 +1,15 @@
-export async function POST(formData) {
-    const req = await fetch(`http://localhost:8080/members/new`, {
+
+
+export async function POST(request) {
+    const formData = await request.json()
+    const res = await fetch(`http://localhost:8080/members/new`, {
+        method: 'POST',
         headers: {
             "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData)
+          },
+        body: {
+            formData
+        }
     })
+    return Response.json(formData)
 }
