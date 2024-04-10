14702ee06aaff58753fc29f16becd7b90321a1b1
honi31 <winner020531@naver.com>
2024-04-10 18:49:07 +0900

    [FEAT]-api

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -1,11 +1,13 @@
-export async function POST(request) {
-  const formData = await request.json();
-  const res = await fetch(`http://localhost:8080/members/new`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  });
-  return res;
+export async function POST(response) {
+    const formData = await response.json()
+    const res = await fetch(`http://localhost:8080/members/new`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+          },
+        body: 
+            formData
+    })
+    const data = await res.json
+    return Request.json(data)
 }
