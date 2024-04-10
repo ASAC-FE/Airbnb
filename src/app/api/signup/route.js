cf4bc1e6a19fa36444168f16c70095f6ded04bc6
luda412 <201810791@sangmyung.kr>
2024-04-10 18:45:47 +0900

    [FEAT]-프록시 버전에서 router로 전환

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -1,15 +1,11 @@
-
-
 export async function POST(request) {
-    const formData = await request.json()
-    const res = await fetch(`http://localhost:8080/members/new`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-          },
-        body: {
-            formData
-        }
-    })
-    return Response.json(formData)
+  const formData = await request.json();
+  const res = await fetch(`http://localhost:8080/members/new`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+  return res;
 }
